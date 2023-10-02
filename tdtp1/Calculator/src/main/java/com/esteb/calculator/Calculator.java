fb0d77cb9a284bbbb75510185ccd62a42c3d9a29
Esteban Dujardin <este22pro@gmail.com>
2023-10-02 15:51:07 +0200

    Ajout du projet FacteurPremiers

--- tdtp1/Calculator/src/main/java/com/esteb/calculator/Calculator.java
+++ tdtp1/Calculator/src/main/java/com/esteb/calculator/Calculator.java
@@ -1,4 +1,24 @@
 package com.esteb.calculator;
 
 public class Calculator {
+    public int add(int opG, int opD) {
+        return opG+opD;
+    }
+
+    public int add2(int opG, int opD) {
+        if ((opG > 0 && opD > Integer.MAX_VALUE )) {
+            throw new ArithmeticException("Cela dépasse la valeur d'un int");
+        }
+        return opG + opD;
+    }
+    public int divide(int opG, int opD) {
+        return opG/opD;
+    }
+
+    public int divide2(int opG, int opD) {
+        if(opD == 0) {
+            throw new ArithmeticException("Division par 0");
+        }
+        return opG/opD;
+    }
 }
--- tdtp1/Calculator/src/test/java/com/esteb/calculator/CalculatorTest.java
+++ tdtp1/Calculator/src/test/java/com/esteb/calculator/CalculatorTest.java
@@ -1,7 +1,86 @@
 package com.esteb.calculator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CalculatorTest {
 
+    private static Calculator calc;
+    @BeforeEach
+    void init(){
+        calc = new Calculator();
+    }
+    @Test
+    void somme_nb_positif(){
+        //Given
+        int nb1 = 12;
+        int nb2 = 12;
+        //When
+        int result = calc.add(nb1, nb2);
+        //Then
+        assertThat(result).isEqualTo(24);
+    }
+    @Test
+    void somme_nb_negatif(){
+        //Given
+        int nb1 = 12;
+        int nb2 = -14;
+        //When
+        int result = calc.add(nb1, nb2);
+        //Then
+        assertThat(result).isEqualTo(-2);
+    }
+
+    @Test
+    void somme_nombres_negatif(){
+        //Given
+        int nb1 = -12;
+        int nb2 = -12;
+        //When
+        int result = calc.add(nb1, nb2);
+        //then
+        assertThat(result).isEqualTo(-24);
+    }
+
+    @Test
+    void division_nb(){
+        //Given
+        int nb1 = 12;
+        int nb2 = 2;
+        //When
+        int result = calc.divide(nb1, nb2);
+        //Then
+        assertThat(result).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0, 1, 1", "1, 2, 3", "-2, 2, 0", "0, 0, 0", "-1, -2, -3"})
+    public void testAdd(int opG, int opD, int resultatVoulu) {
+        int result = calc.add(opG, opD);
+        assertEquals(resultatVoulu, result);
+    }
+
+    @Test
+    void testAddWithOverflow() {
+
+        assertThatThrownBy(() -> calc.add2(1, Integer.MAX_VALUE))
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage("OVERFLOW");
+    }
+
+    @Test
+    void testDivide0(){
+
+    }
+    @AfterEach
+    void after(){
+        calc = null;
+    }
 }
