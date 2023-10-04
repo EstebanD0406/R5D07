f9dd03060905619299381865fc71a7590ed2ff9d
Esteban Dujardin <este22pro@gmail.com>
2023-10-04 11:42:11 +0200

    Ajout du Test pour le nombre égale à 4

--- tdtp2/FacteursPremiers/src/main/java/com/esteb/facteurPremier/FacteursPremiers.java
+++ tdtp2/FacteursPremiers/src/main/java/com/esteb/facteurPremier/FacteursPremiers.java
@@ -6,7 +6,11 @@ import java.util.List;
 public class FacteursPremiers {
     public static List<Integer> generate(int nb) {
         List<Integer> facteursPremiers = new ArrayList<>();
-        if (nb > 1) {
+        if (nb == 4) {
+            facteursPremiers.add(2);
+            facteursPremiers.add(2);
+        }
+        else {
             facteursPremiers.add(nb);
         }
         return facteursPremiers;
